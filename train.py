47e4e38ff6f5b99d6a598e04ab5ecdacb49d2460
Antonius Danu Pranoto <antoniusdanu600@gmail.com>
2025-03-19 09:02:05 +0700

    Update train.py

--- train.py
+++ train.py
@@ -12,3 +12,14 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
 results = model.train(data=os.path.join('/content/gdrive/My Drive/Plat_sepeda_motor_yolov8/train', "google_collab.yaml"), epochs=100)  # train the model
+
+# use the code below on separate cell to download model to your gdrive
+import locale
+import os
+
+def getpreferredencoding(do_setlocale = True):
+    return "UTF-8"
+
+locale.getpreferredencoding = getpreferredencoding
+
+!scp -r /content/runs '/content/gdrive/My Drive/Plat_sepeda_motor_yolov8'
